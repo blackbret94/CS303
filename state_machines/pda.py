1910164643e0f4314f3c8089ade1d1c45ff08a2e
Bret <emomartian@gmail.com>
2015-03-11 00:49:22 -0400

    Added the basis of a PDA

---
+++ state_machines/pda.py
@@ -0,0 +1,21 @@
+"""  PDA CREATOR  """
+""" BY BRET BLACK """
+
+#####################
+#   START PROGRAM   #
+#####################
+
+import stateMachine
+
+
+# defines a PDA machine
+class PDA:
+	# blank constructor
+	def __init__(self):
+		# do stuff
+
+	# accepts seven inputs
+	def __init__(self,states,inputSymbols,stackAlphabet,transitionFunction,startState,startSymbol,acceptingStates):
+		# do stuff
+
+# defines a transition function
